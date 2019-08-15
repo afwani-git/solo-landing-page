e11588de451ec50e38db6dcee6009630dbe93a4b
afwani shofiyulloh <afwani123@gmail.com>
2019-08-15 09:56:10 +0700

    write team section and add plugin jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-// $(function(){
-// 	$("#preloader").delay().hide();
-// });
-
+// load
 $(window).on('load',function(){
 	$("#status").fadeOut();
 	$("#preloader").delay(450).fadeOut();
 });
+// member
+$(function(){
+	$("#team-members").owlCarousel({
+		items:2,
+		autoplay:true,
+		loop:true,
+		smartSpeed:700,
+		autoplayHoverPause:true,
+		nav:true,
+		dots:false,
+		navText:['<i class=" ico fa fa-angle-left"></i>','<i class=" ico fa fa-angle-right"></i">']
+	});
+})
