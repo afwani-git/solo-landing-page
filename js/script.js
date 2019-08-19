493c5397d84acf9b6d9951cb9ea5a3bd524eadc1
afwani shofiyulloh <afwani123@gmail.com>
2019-08-19 21:52:57 +0700

    corat coret 3 section html + js selesai

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// load
 $(window).on('load',function(){
 	$("#status").fadeOut();
 	$("#preloader").delay(450).fadeOut();
 });
-// member
 $(function(){
 	$("#team-members").owlCarousel({
 		items:2,
@@ -16,7 +14,6 @@ $(function(){
 		navText:['<i class=" ico fa fa-angle-left"></i>','<i class=" ico fa fa-angle-right"></i">']
 	});
 })
-// progress Bar
 $(function(){
 	$("#progress-element").waypoint(function(){
 		$(".progress-bar").each(function() {
@@ -29,7 +26,6 @@ $(function(){
 		offset:'bottom-in-view'
 	});
 })
-
 $(function(){
 	$("#team-members").owlCarousel({
 		items:2,
@@ -42,11 +38,9 @@ $(function(){
 		navText:['<i class=" ico fa fa-angle-left"></i>','<i class=" ico fa fa-angle-right"></i">']
 	});
 })
-// progress Bar
 $(".se").responsiveTabs({
 	animation:'slide'
 });
-// isotope-filter
 $(window).on('load',function(){
 	$(".isotope-container").isotope({});
 	$('.isotope-filter').on('click','button',function(){
@@ -66,4 +60,28 @@ $(function(){
 			enabled:true
 		}
 	})
+})
+$(function(){
+	$("#testimonial-slinder").owlCarousel({
+		items:1,
+		autoplay:true,
+		loop:true,
+		smartSpeed:700,
+		autoplayHoverPause:true,
+		nav:true,
+		dots:false,
+		navText:['<i class=" ico fa fa-angle-left"></i>','<i class=" ico fa fa-angle-right"></i">']
+	});
+});
+$(function(){
+	$("#client-list").owlCarousel({
+		items:6,
+		autoplay:true,
+		loop:true,
+		smartSpeed:500,
+		autoplayHoverPause:true,
+		nav:true,
+		dots:false,
+		navText:['<i class=" ico fa fa-angle-left"></i>','<i class=" ico fa fa-angle-right"></i">']
+	});
 })
